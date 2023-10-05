aa4166869ca98f32c7f50acfa3602b516fb25ce6
LuanVM1810 <Vomongluan@gmail.com>
2023-10-05 22:11:28 +0700

    update main() & add new readme.md

--- src/pages/docaPage.tsx
+++ src/pages/docaPage.tsx
@@ -1,9 +1,9 @@
 import { Container, Typography, colors } from "@mui/material";
 import { red } from "@mui/material/colors";
 import styled from "@emotion/styled";
-import Slidebar from "../components/slidebar/Sidebar";
+import Slidebar from "../components/sidebar/Sidebar";
 import PrimarySearchAppBar from "../components/searchbar/SearchBar";
-import SlideBar from "../components/slidebar/Sidebar";
+import SlideBar from "../components/sidebar/Sidebar";
 import Post from "../components/post/Post";
 
 const tilleStyle = { 
