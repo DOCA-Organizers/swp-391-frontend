d16918009dd8699f91a90d79d83b68500e6be7f1
LuanVM1810 <Vomongluan@gmail.com>
2023-10-13 10:25:29 +0700

    update docaPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { CssBaseline } from "@mui/material";
 import { ROUTE_PATH } from "configs";
 import DocaPage from "pages/docaPage";
-import ProfilePage from "pages/profilePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AdminRoutes from "routes/AdminRoutes";
 
@@ -15,8 +14,7 @@ function App() {
           path={ROUTE_PATH.ADMIN}
           element={<AdminRoutes user={} isAllowed={true}/>}
         /> */}
-        <Route path="my_profile"
-             element={<ProfilePage/>}/>
+        
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/profile/createPost.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const createPost = (props: Props) => {
+  return (
+    <div>createPost</div>
+  )
+}
+
+export default createPost
